2c9f51266303a244955359ce6eecccf1b28bd7f8
Zenith-hasan <codiversitydev@gmail.com>
2025-07-26 23:44:52 +0600

    more basic teriminolgy discussion

--- 01-Basic/01-Terminology.sol
+++ 01-Basic/01-Terminology.sol
@@ -9,3 +9,19 @@
 // Web3
 -- Web3 is the next evolution of the internet, built on decentralized technology like blockchain,cryptocurrency,smart contracts and peer-to-peer network.
 
+// Mining 
+-- The process of validating transactions and adding them to the blockchain. Miners solve complex math problems to earn reward usually cryptocurrency.
+
+// Consensus Mechanism 
+-- A rule that ensure all node in the network agree on the blockchain state. For example proof of work (POW) where miners solve puzzle and proof of stake(POS) where validators as chose based on their coin holding.
+
+
+// Gas 
+-- A fee paid to process transactions or smart contracts on network like ethereum.It prevent spam and manages network resources like a toll for using the blockchain.
+
+
+// Stable Coin 
+-- A stable coin is a type of cryptocurrency that is designed to main a statble value, usually by being pegged to a reserve asset such as Fiat Currency(USD,EUR) or commodity(Gold) or even other crytpocurrencies.
+
+
+
