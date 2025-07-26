840aa5a279f15dacaf0c20d6f505c2801d7de409
Zenith-hasan <abuhasan323382@gmail.com>
2025-07-26 23:19:22 +0600

    basic terminology

---
+++ 01-Basic/01-Terminology.sol
@@ -0,0 +1,11 @@
+// Smart Contract 
+-- Smart Contracts are a set of instructions executed in a decentralized way without the need for a centralized or third party intermediary.
+
+
+// Blockchain
+-- A blockchain is a distrubuted digital ledger-a continiously growing chain of data records(called blocks) that are cyptographically liked to the provious one, making the history temper-resistant.
+
+
+// Web3
+-- Web3 is the next evolution of the internet, built on decentralized technology like blockchain,cryptocurrency,smart contracts and peer-to-peer network.
+
